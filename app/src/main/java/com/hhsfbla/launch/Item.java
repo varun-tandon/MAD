8b2a0b22326cff82149231816aafc4696e056f8f
Michael <chen.michael9@gmail.com>
2017-01-31 00:16:41 -0800

    Fundraiser Updates

---
+++ app/src/main/java/com/hhsfbla/launch/Item.java
@@ -0,0 +1,19 @@
+package com.hhsfbla.launch;
+
+/**
+ * Created by zhenfangchen on 1/30/17.
+ */
+
+public class Item {
+
+    protected String name;
+    protected double price;
+    protected int stock;
+
+    public Item(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+}
