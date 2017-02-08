488f11073c0fb587080d86ee9bd14e77ddea0dab
Heidi Dong <dong.heidi@gmail.com>
2017-02-07 19:10:59 -0800

    Update item ui with back button and minor visual changes

--- app/src/main/java/com/hhsfbla/launch/ItemActivity.java
+++ app/src/main/java/com/hhsfbla/launch/ItemActivity.java
@@ -3,6 +3,8 @@ package com.hhsfbla.launch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by zhenfangchen on 2/7/17.
@@ -12,11 +14,24 @@ public class ItemActivity extends AppCompatActivity{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_item);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.donate_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Intent intent = getIntent();
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
