004971a4f28c05d185b8cb220a6a3cc405944b70
Heidi Dong <dong.heidi@gmail.com>
2017-02-08 15:58:50 -0800

    Set datepicker min date to current date

--- app/src/main/java/com/hhsfbla/launch/CreateFundraiserActivity.java
+++ app/src/main/java/com/hhsfbla/launch/CreateFundraiserActivity.java
@@ -1,17 +1,11 @@
 package com.hhsfbla.launch;
 
-import android.app.ActionBar;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
